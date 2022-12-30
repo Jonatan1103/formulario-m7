9d11bd44b07d8b5a5d0d47a59c3114d98bf6dc8a
jonatan timoteo dos santos <jonatan_ts@hotmail.com>
2022-12-30 12:25:36 -0300

    added: funcionalidades

---
+++ main.js
@@ -0,0 +1,27 @@
+const inputA = document.getElementById('input-value-a')
+const inputB = document.getElementById('input-value-b')
+const formNumber = document.getElementById('formulario-numero')
+const buttonSendNumber = document.getElementById('btn-send-number')
+const msgSuccess = document.querySelector('.message')
+
+formNumber.addEventListener('submit', (event) => {
+  event.preventDefault()
+  const valueInputA = inputA.value
+  const valueInputB = inputB.value
+
+  const BMaiorQueA = valueInputA < valueInputB
+
+  if (BMaiorQueA) {
+    msgSuccess.classList.remove('msg-error')
+    msgSuccess.classList.add('msg-success')
+    msgSuccess.innerHTML = ''
+    msgSuccess.innerHTML = `Números <strong>${valueInputA}</strong> e <strong>${valueInputB}</strong> enviados com sucesso`
+
+    inputA.value = ''
+    inputB.value = ''
+  } else {
+    msgSuccess.classList.add('msg-error')
+    msgSuccess.innerHTML = ''
+    msgSuccess.innerHTML = `Campo <strong>A</strong> deve ser menor que Campo <strong>B</strong>`
+  }
+})
